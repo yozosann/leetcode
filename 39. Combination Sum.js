712ddc2c240986527870febd99af495dc862b77b
tianjie <ytj123yue_@126.com>
2017-07-31 00:49:50 +0800

    139

--- 39. Combination Sum.js
+++ 39. Combination Sum.js
@@ -1,13 +1,26 @@
 let combinationSum = function (candidates, target) {
-  let results = [];
-  let newCandidates = candidates.filter((val) => {
-    if (val === target) {
-      results.push([val]);
+  let result = [];
+  for (let val of candidates) {
+    if (val > target) continue;
+    if (target == val) {
+      result.push([val]);
+      continue;
     }
-    return val < target;
-  });
-  console.log(newCandidates);
-  return results;
+    concat(combinationSum(candidates, val), combinationSum(candidates, target - val), result);
+  }
+  if (result.length) {
+    return result;
+  } else {
+    return false;
+  }
 };
 
 console.log(combinationSum([2, 3, 4, 7], 7));
+
+function concat(arr1, arr2, result) {
+  arr1 && arr1.forEach(val1 => {
+    arr2 && arr2.forEach(val2 => {
+      val1 && val2 && result.push(val1.concat(val2));
+    })
+  })
+}
