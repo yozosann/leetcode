e3e7f1d2315e240a97d4e12225038b0d757eddf7
yangtianjie <yangtianjie@meituan.com>
2017-07-31 20:04:05 +0800

    39

--- 39. Combination Sum.js
+++ 39. Combination Sum.js
@@ -1,11 +1,13 @@
 let combinationSum = function (candidates, target) {
   let result = [];
+  let duplicate = [];
   for (let val of candidates) {
     if (val > target) continue;
     if (target == val) {
       result.push([val]);
       continue;
     }
+    duplicate.push(target - val);
     concat(combinationSum(candidates, val), combinationSum(candidates, target - val), result);
   }
   if (result.length) {
@@ -15,12 +17,52 @@ let combinationSum = function (candidates, target) {
   }
 };
 
-console.log(combinationSum([2, 3, 4, 7], 7));
+for (let val of combinationSum([2, 3, 4, 7], 7)) {
+  console.log(val);
+}
 
 function concat(arr1, arr2, result) {
   arr1 && arr1.forEach(val1 => {
     arr2 && arr2.forEach(val2 => {
-      val1 && val2 && result.push(val1.concat(val2));
+      var resArr = val1.concat(val2);
+      if (noDuplicate(result, resArr)) {
+        result.push(resArr);
+      }
     })
   })
+}
+
+function noDuplicate(result, target) {
+  if (!result.length) return true;
+  let isDuplicate = false;
+  for (let val of result) {
+    if (val.length == target.length) {
+      isDuplicate = compare(sumUp(val), sumUp(target));
+      if (isDuplicate) break;
+    } else {
+      continue;
+    }
+  };
+  return !isDuplicate;
+}
+
+function sumUp(arr) {
+  let obj = {};
+  if (!arr.length) return obj;
+  for (let val of arr) {
+    if (obj[val]) {
+      obj[val]++;
+    } else {
+      obj[val] = 1;
+    }
+  };
+  return obj;
+}
+
+function compare(obj1, obj2) {
+  for (let i in obj1) {
+    if (obj1[i] == obj2[i]) continue;
+    else return false;
+  }
+  return true;
 }
